f0343e93d70ccb978718fd2d90cdab02d3cb8abb
danimtb <danimanzaneque@gmail.com>
2017-12-01 10:26:53 +0100

    Adds real code to test_package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,5 +1,16 @@
 #include <GLFW/glfw3.h>
 
-int main (void){
-  return 0;
+int main (void)
+{
+    GLFWwindow* window;
+    GLFWmonitor* monitor = NULL;
+
+    monitor = glfwGetPrimaryMonitor();
+
+    window = glfwCreateWindow(640, 480, "Window name", monitor, NULL);
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
 }
